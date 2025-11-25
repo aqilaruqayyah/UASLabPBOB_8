eaf507864b662552e5702117fdaa00638c26ae32
ken <arkanzubir@gmail.com>
2025-11-25 07:59:15 +0700

    Create MenuItem.java

---
+++ RestaurantSystem/src/models/menu/MenuItem.java
@@ -0,0 +1,24 @@
+package  models.menu;
+
+public abstract class MenuItem {
+    private String nama;
+    private double harga;
+
+    public MenuItem(String nama, double harga) {
+        this.nama = nama;
+        this.harga = harga;
+    }
+
+    public abstract String getInfo();
+
+    public String getNama() {
+        return nama;
+    }
+
+    
+    public double getHarga() {
+        return harga;
+    }
+
+
+} 
